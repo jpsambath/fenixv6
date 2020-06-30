857ac80fa53d2e30701e195c38edfcdc70c5ac84
Jipoon <jp@karmage.fr>
2020-06-30 18:06:21 +0200

    ok v3

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -18,7 +18,7 @@ class AdminController extends AbstractController
     /**
      * @Route("/synchronizeprintify", name="synchronizeprintify")
      */
-    public function index()
+    public function synchronizeprintify()
     {
         if ($this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_FULLY') AND $this->get('security.authorization_checker')->isGranted('ROLE_ADMIN')) {
             $em = $this->getDoctrine()->getManager();
@@ -75,4 +75,66 @@ class AdminController extends AbstractController
 
         return $this->render('admin/synchronizeprintify.html.twig');
     }
+
+    /**
+     * @Route("/synchronizeprintful", name="synchronizeprintful")
+     */
+    public function synchronizeprintful()
+    {
+        if ($this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_FULLY') AND $this->get('security.authorization_checker')->isGranted('ROLE_ADMIN')) {
+            $em = $this->getDoctrine()->getManager();
+            $security = new Security($this->container);
+
+            $printify = new Printful($security, $em);
+
+            // RETRIEVE ALL SHOP FOR THIS USER
+            $shoplist = $printify->retrieveshoplist();
+
+//            // RETRIEVE ALL PROVIDERS FROM PRINTIFY
+//            $providerlist = $printify->retrieveproviderlist();
+//
+//            // RETRIEVE ALL BLUEPRINTS FROM PRINTIFY
+//            $blueprintlist = $printify->retrieveblueprintlist();
+//
+//            // RETRIEVE LINK BETWEEN BLUEPRINTS AND PROVIDERS FROM BLUEPRINT SIDE (PROVIDERS' SIDE IS NOT RELIABLE)
+//            $blueprintlist = $this->getDoctrine()->getRepository(Blueprint::class)->findAll();
+//            $fullblueprintlist = array();
+//            foreach ($blueprintlist as $blueprint) {
+//                $fullblueprintlist[] = $printify->retrieveblueprintprovider($blueprint->getId());
+//            }
+//
+//            // RETRIEVE ALL SHIPPINGS INFO BASED ON PROVIDERS AND BLUEPRINTS
+//            $fullproviderlist = $this->getDoctrine()->getRepository(Provider::class)->findAll();
+//            foreach ($fullproviderlist as $fullprovider) {
+//                foreach ($fullprovider->getBlueprints() as $fullproviderblueprint) {
+//                    $shippinglist[] = $printify->retrieveshipping($fullproviderblueprint->getId(), $fullprovider->getId());
+//                }
+//            }
+
+//
+//             RETRIEVE ALL VARIANTS BASED ON PROVIDERS AND BLUEPRINTS
+            $fullproviderlist = $this->getDoctrine()->getRepository(Provider::class)->findAll();
+            $variantslist = array();
+            foreach($fullproviderlist as $fullprovider){
+                foreach($fullprovider->getBlueprints() as $fullproviderblueprint){
+                    $variantslist[] = $printify->retrievevariants($fullproviderblueprint->getId(), $fullprovider->getId());
+                }
+            }
+
+//            $variantslist[] = $printify->retrievevariants(14, 25);
+//            $variantslist[] = $printify->retrievevariants(14, 16);
+
+            return $this->render('admin/synchronizeprintify.html.twig', [
+                'printify_shoplist' => $shoplist,
+//                'printify_blueprintlist' => $blueprintlist,
+//                'printify_providerlist' => $providerlist,
+//                'printify_fullblueprintlist' => $fullblueprintlist,
+//               'printify_shippinglist' => $shippinglist,
+                'printify_variantlist' => $variantslist,
+            ]);
+        }
+
+        return $this->render('admin/synchronizeprintify.html.twig');
+    }
+
 }
--- src/Service/Printful.php
+++ src/Service/Printful.php
@@ -4,7 +4,60 @@
 namespace App\Service;
 
 
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Security;
+
 class Printful
 {
+    private const BASE_URL = "https://api.printify.com/";
 
+    private $headers = [];
+
+    private $em;
+
+    private $security;
+
+    /**
+     * Printify constructor.
+     * @param Security $security
+     * @param ObjectManager $em
+     */
+    public function __construct(Security $security, EntityManagerInterface $em)
+    {
+        $this->em = $em;
+        $this->security = $security;
+        $this->headers = array();
+        $this->addHeaders("Authorization", "Bearer " . $this->security->getUser()->get());
+        $this->addHeaders("Content-Type", "application/json");
+    }
+
+    /**
+     * @return array
+     */
+    public function getHeaders(): array
+    {
+        return $this->headers;
+    }
+
+    /**
+     * @param array $headers
+     * @return Printify
+     */
+    public function setHeaders(array $headers): Printify
+    {
+        $this->headers = $headers;
+        return $this;
+    }
+
+    /**
+     * @param string $index
+     * @param string $value
+     * @return Printify
+     */
+    public function addHeaders(string $index, string $value): Printify
+    {
+        $this->headers[$index] = $value;
+        return $this;
+    }
 }
