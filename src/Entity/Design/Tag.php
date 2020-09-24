5cb7e09a8ccb2d90c98f03d9f7a97601cb5cb409
Jipoon <jp@karmage.fr>
2020-09-24 14:54:44 +0200

    ok v10

--- src/Entity/Design/Tag.php
+++ src/Entity/Design/Tag.php
@@ -2,6 +2,8 @@
 
 namespace App\Entity\Design;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -21,6 +23,22 @@ class Tag
      */
     private $name;
 
+    /**
+     * @ORM\ManyToMany(targetEntity="App\Entity\Design\Tag", inversedBy="children")
+     */
+    private $parent;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="App\Entity\Design\Tag", mappedBy="parent")
+     */
+    private $children;
+
+    public function __construct()
+    {
+        $this->parent = new ArrayCollection();
+        $this->children = new ArrayCollection();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
@@ -34,6 +52,60 @@ class Tag
     public function setName(string $name): self
     {
         $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|self[]
+     */
+    public function getParent(): Collection
+    {
+        return $this->parent;
+    }
+
+    public function addParent(self $parent): self
+    {
+        if (!$this->parent->contains($parent)) {
+            $this->parent[] = $parent;
+        }
+
+        return $this;
+    }
+
+    public function removeParent(self $parent): self
+    {
+        if ($this->parent->contains($parent)) {
+            $this->parent->removeElement($parent);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|self[]
+     */
+    public function getChildren(): Collection
+    {
+        return $this->children;
+    }
+
+    public function addChild(self $child): self
+    {
+        if (!$this->children->contains($child)) {
+            $this->children[] = $child;
+            $child->addParent($this);
+        }
+
+        return $this;
+    }
+
+    public function removeChild(self $child): self
+    {
+        if ($this->children->contains($child)) {
+            $this->children->removeElement($child);
+            $child->removeParent($this);
+        }
 
         return $this;
     }
